84626bfaa82ca2d232b635f4c9b1563c535ba0d7
Matheus de Mello Vieira <mellomatheus67@gmail.com>
2024-08-20 00:01:15 -0300

    refactored tests

--- src/tests/events.test.js
+++ src/tests/events.test.js
@@ -1,5 +1,4 @@
 import { handler as createEvent } from "../handlers/create-event.js";
-import { getUserId } from "../utilities/request.js";
 import { assembleHandleResponse } from "../utilities/response.js";
 import { morningEvent } from "./events.mother.js";
 
@@ -9,11 +8,9 @@ describe("Create", () => {
   test("should insert an item into DynamoDB", async () => {
     const event = morningEvent;
 
-    const lambdaEvent = {
+    const response = await createEvent({
       body: event.asJson(),
-    };
-
-    const response = await createEvent(lambdaEvent);
+    });
 
     expect(response).toEqual(
       assembleHandleResponse(201, {
@@ -23,22 +20,4 @@ describe("Create", () => {
 
     await event.expectHasEqualOnDatabase();
   });
-
-  // test("should delete an item from DynamoDB", async () => {
-  //   const params = {
-  //     TableName: "YourTableName",
-  //     Key: { id: "1", sortKey: "SK1" },
-  //   };
-
-  //   await dynamoDB.delete(params).promise();
-
-  //   const result = await dynamoDB
-  //     .get({
-  //       TableName: "YourTableName",
-  //       Key: { id: "1", sortKey: "SK1" },
-  //     })
-  //     .promise();
-
-  //   expect(result.Item).toBeUndefined();
-  // });
 });
