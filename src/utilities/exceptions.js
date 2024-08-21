a10be7c3152cb1d942e62d32cff2bd79886d3a2e
Matheus de Mello Vieira <mellomatheus67@gmail.com>
2024-08-21 11:08:38 -0300

    created userError

--- src/utilities/exceptions.js
+++ src/utilities/exceptions.js
@@ -1,12 +1,12 @@
 export const UserError = class {
   constructor(responseBody) {
-    this.responseBody;
-  }
-};export const ValidationError = class {
-  constructor(errors, target) {
-    this.output = {
-      [target]: errors,
-    };
+    this.responseBody = responseBody;
   }
 };
-
+export const ValidationError = class extends UserError {
+  constructor(errors, target) {
+    super({
+      [target]: errors,
+    });
+  }
+};
--- src/utilities/handlerWrapper.js
+++ src/utilities/handlerWrapper.js
@@ -1,13 +1,13 @@
 import { assembleHandleResponse } from "../utilities/response.js";
-import { ValidationError } from "./exceptions.js";
+import { UserError } from "./exceptions.js";
 
 export const wrapHandler = (handler) => {
   return async (event) => {
     try {
       return await handler(event);
     } catch (error) {
-      if (error instanceof ValidationError) {
-        return assembleHandleResponse(400, error.output);
+      if (error instanceof UserError) {
+        return assembleHandleResponse(400, error.responseBody);
       }
 
       console.error(error);
